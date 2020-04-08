4f08bad7bc06eb020c55197c30ec259cc75bdcf9
Tyler <tyler.li@mail.utoronto.ca>
2020-04-08 17:00:32 -0400

    Add files via upload
    Updated version

--- PS5/submit-script.R
+++ PS5/submit-script.R
@@ -24,19 +24,19 @@ dt <- dt_raw %>%
 #########################
 # Summary stats
 #########################
-# sanity check
+# sanity check of original dataset
 skim(dt_raw)
 
-# how many total data points
+# how many total data points after filtering
 nrow(dt)
 
-# how many unique values
+# how many unique values after filtering
 length(unique(dt$X))
 
-# frequency distribution of the running variable
+# frequency distribution of the running variable X
 ggplot(dt, aes(x=X)) +
-  geom_histogram(breaks=seq(-1.3, 0, by = 0.1), col="black", fill="pink", alpha = 1)+
-  geom_histogram(breaks=seq(0, 1.3, by = 0.1), col="black", fill="orange", alpha = 1)+
+  geom_histogram(breaks=seq(-1.6, 0, by = 0.1), col="black", fill="pink", alpha = 1)+
+  geom_histogram(breaks=seq(0, 1.6, by = 0.1), col="black", fill="orange", alpha = 1)+
   geom_vline(aes(xintercept=0),   # Ignore NA values for mean
              color="red", linetype="dashed", size=1) +
   ggtitle("Frequency distribution of the running variable (distance from GPA cutoff)") +
@@ -52,14 +52,15 @@ ggplot(dt, aes(x=X)) +
 density_check <- rddensity(dt$X)
 summary(density_check)
 
-# plot test
-rdplotdensity(density_check, dt$X, xlabel = "Distance to the GPA cut-off", ylabel = "Density", title = "Density plot of X (Distance to the GPA cut-off)")
+# Density plot test
+rdplotdensity(density_check, dt$X, xlabel = "Distance to the GPA cut-off",
+              ylabel = "Density", title = "Density plot of X (Distance to the GPA cut-off)")
 
 
 ## Continuous for other revelent variables ##
 
-# those summaries provides raw outputs
-# we created table 5.2.2 manually by inserting the results
+# These summaries provide raw outputs
+# We created table 5.2.2 manually by inserting the results
 summary(rdrobust(dt$hsgrade_pct, dt$X))
 summary(rdrobust(dt$totcredits_year1, dt$X))
 summary(rdrobust(dt$age_at_entry, dt$X))
@@ -71,34 +72,33 @@ summary(rdrobust(dt$loc_campus2, dt$X))
 summary(rdrobust(dt$loc_campus3, dt$X))
 
 #########################
-# Model
+# Model and Results
 #########################
 # Probation on Immediate decision, by gender
 
 ## FEMALE ##
 female_rdd <- dt %>% filter(male == 0)
-rdplot(female_rdd$left_school, female_rdd$X,  x.label = "Distance to GPA cut-off", y.label = "Left University Voluntariliy", title="Effect of Academic Probation on Attrition, Female", col.dots="red")
+rdplot(female_rdd$left_school, female_rdd$X,  x.label = "Distance to GPA cut-off",
+       y.label = "Left University Voluntariliy", title="Effect of Academic Probation on Attrition, Female", col.dots="red")
 summary(rdrobust(female_rdd$left_school, female_rdd$X))
 
 ## MALE ##
 male_rdd <- dt %>% filter(male == 1)
-rdplot(male_rdd$left_school, male_rdd$X,  x.label = "Distance to GPA cut-off", y.label = "Left University Voluntariliy", title="Effect of Academic Probation on Attrition, Male", col.dots="red")
+rdplot(male_rdd$left_school, male_rdd$X,  x.label = "Distance to GPA cut-off", 
+       y.label = "Left University Voluntariliy", title="Effect of Academic Probation on Attrition, Male", col.dots="red")
 summary(rdrobust(male_rdd$left_school, male_rdd$X))
 
 # Probation on Immediate decision, by native-language
 
 ## ENGLISH ##
 eng_rdd <- dt %>% filter(english == 1)
-rdplot(eng_rdd$left_school, eng_rdd$X,  x.label = "Distance to GPA cut-off", y.label = "Left University Voluntariliy", title="Effect of Native Language on Attrition, English", col.dots="red")
+rdplot(eng_rdd$left_school, eng_rdd$X,  x.label = "Distance to GPA cut-off", 
+       y.label = "Left University Voluntariliy", title="Effect of Native Language on Attrition, English", col.dots="red")
 summary(rdrobust(eng_rdd$left_school, eng_rdd$X))
 
 ## NON-ENGLISH ##
 non_eng_rdd <- dt %>% filter(english == 0)
-rdplot(non_eng_rdd$left_school, non_eng_rdd$X,  x.label = "Distance to GPA cut-off", y.label = "Left University Voluntariliy", title="Effect of Native Language on Attrition, Non-English", col.dots="red")
+rdplot(non_eng_rdd$left_school, non_eng_rdd$X,  x.label = "Distance to GPA cut-off", 
+       y.label = "Left University Voluntariliy", title="Effect of Native Language on Attrition, Non-English", col.dots="red")
 summary(rdrobust(non_eng_rdd$left_school, non_eng_rdd$X))
-
-
-
-
-
 
